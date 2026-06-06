d247c131edce5205dabc015bca0e2d33b88f11f6
lporcsin <porcsin@pm.me>
2026-06-06 19:03:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const images = [
   { file: "torta1.jpg", cat: "torta", title: "Gesztenye torta" },
   { file: "torta2.jpg", cat: "torta", title: "Mákosguba torta" },
   { file: "torta3.jpg", cat: "torta", title: "Erdei gyümölcsös fehércsokis túrótorta" },
+  { file: "torta4.jpg", cat: "torta", title: "Fagyitorta" },
 ];
 
 const gallery = document.getElementById("gallery");
